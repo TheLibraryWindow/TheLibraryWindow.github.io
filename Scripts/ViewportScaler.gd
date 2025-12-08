a8a79e5a446d9042373424e17bc87423e160e906
Conor <TheLibraryWindow@proton.me>
2025-12-08 15:57:30 +0100

    Init 2

--- Scripts/ViewportScaler.gd
+++ Scripts/ViewportScaler.gd
@@ -2,6 +2,7 @@ extends Node
 
 const LANDSCAPE := Vector2i(1280, 720)
 const PORTRAIT := Vector2i(720, 1280)
+const MOBILE_SHORT_EDGE_THRESHOLD := 820
 
 func _ready() -> void:
 	_update_viewport()
@@ -11,6 +12,14 @@ func _notification(what: int) -> void:
 		_update_viewport()
 
 func _update_viewport() -> void:
-	var win_size := DisplayServer.window_get_size()
-	var target := PORTRAIT if win_size.x < win_size.y else LANDSCAPE
-	get_viewport().content_scale_size = target
+	var viewport: Viewport = get_viewport()
+	var win_size: Vector2i = DisplayServer.window_get_size()
+	if win_size == Vector2i.ZERO:
+		return
+
+	var target: Vector2i = PORTRAIT if win_size.x < win_size.y else LANDSCAPE
+	var shortest_edge: int = min(win_size.x, win_size.y)
+	if shortest_edge <= MOBILE_SHORT_EDGE_THRESHOLD:
+		viewport.content_scale_size = win_size
+	else:
+		viewport.content_scale_size = target
